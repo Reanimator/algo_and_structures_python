6212de68df4e6f165affcf3b422fc3b0d6056c63
Reanimator <ageev.mikhail.23@gmail.com>
2020-01-13 18:18:05 +0300

    Add remain

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -1,3 +1,7 @@
+__author__ = 'Агеев Михаил Михайлович'
+
+import random
+
 """
 4.	Написать программу, которая генерирует в указанных пользователем границах
 ●	случайное целое число,
@@ -8,3 +12,32 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+NUM_INT_1 = int(input("Введите начало диапазона для целого числа: \n"))
+NUM_INT_2 = int(input("Введите конец диапазона для целого числа: \n"))
+NUM_REAL_1 = int(input("Введите начало диапазона для вещественного числа: \n"))
+NUM_REAL_2 = int(input("Введите конец диапазона для вещественного числа: \n"))
+SYM_1 = input("Введите начало диапазона для символов: \n")
+SYM_2 = input("Введите конец диапазона для символов: \n")
+SYMBOLS = "abcdefghijklmnopqrstuvwxyz"
+
+if NUM_INT_1 < NUM_INT_2:
+    print(
+        f"Рандомное целое число из диапазона: {random.randint(NUM_INT_1, NUM_INT_2)}")
+else:
+    print(
+        f"Рандомное целое число из диапазона: {random.randint(NUM_INT_2, NUM_INT_1)}")
+
+if NUM_INT_1 < NUM_INT_2:
+    print(
+        f"Рандомное вещественное число из диапазона: {random.uniform(NUM_REAL_1, NUM_REAL_2)}")
+else:
+    print(
+        f"Рандомное вещественное число из диапазона: {random.uniform(NUM_REAL_2, NUM_REAL_1)}")
+
+NUM_SYM_1 = SYMBOLS.find(SYM_1)
+NUM_SYM_2 = SYMBOLS.find(SYM_2)
+if NUM_SYM_1 < NUM_SYM_2:
+    print(f"Рандомный символ: {SYMBOLS[random.randint(NUM_SYM_1, NUM_SYM_2)]}")
+else:
+    print(f"Рандомный символ: {SYMBOLS[random.randint(NUM_SYM_2, NUM_SYM_1)]}")
--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,17 @@
+__author__ = 'Агеев Михаил Михайлович'
+
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+SYM_1 = input("Введите первую букву: \n")
+SYM_2 = input("Введите вторую букву: \n")
+SYMBOLS = "abcdefghijklmnopqrstuvwxyz"
+
+NUM_SYM_1 = SYMBOLS.find(SYM_1)
+print(f"Первая буква стоит на {NUM_SYM_1 + 1} месте.")
+NUM_SYM_2 = SYMBOLS.find(SYM_2)
+print(f"Первая буква стоит на {NUM_SYM_2 + 1} месте.")
+if NUM_SYM_1 > NUM_SYM_2:
+    print(f"Между ними {NUM_SYM_1 - NUM_SYM_2 - 1} букв")
+else:
+    print(f"Между ними {NUM_SYM_2 - NUM_SYM_1 - 1} букв")
